69437008049cd457b0261f2dc6b182d2f9ef3117
Ashwani <ashwanidey2904@gmail.com>
2024-10-04 13:05:59 +0530

    Added Courses

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import NumberCounter from './components/NumberCounter/NumberCounter'
 import { Route, Routes } from "react-router-dom";
 import HomePage from './pages/HomePage/HomePage'
 import ProductsPage from './pages/ProductsPage/ProductsPage'
+import Course from './pages/CoursesPage/CoursePage'
 
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/products" element={<ProductsPage />} />
+          <Route path="/courses" element={<Course />} />
         </Routes>
       
       </main>
---
+++ frontend/src/pages/CoursesPage/CoursePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CoursePage = () => {
+  return (
+    <div className='container'>
+      <video className='md:w-[90%]' controls>
+        <source src="https://d1pghkbs868271.cloudfront.net/2020-10-05%2022-21-43.mp4" type="video/mp4" />
+      </video>
+    </div>
+  )
+}
+
+export default CoursePage
