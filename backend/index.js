48e7e39fdc3def5f8fb839c737deef0e69f7e657
Ashwani <ashwanidey2904@gmail.com>
2024-11-12 20:34:48 +0530

    Serving Static Files

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import authRoutes from "./routes/authRoute.js";
+import axios from "axios";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 
 
@@ -19,16 +22,48 @@ app.use(cors());
 
 
 const PORT = process.env.PORT || 3001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files based on environment
+if (process.env.NODE_MODE === "production") {
+  // Serve static files from the React app in production
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+} else {
+  // Serve a message indicating the API is running in development
+  app.get("/", (req, res) => {
+    res.send("API is running in development mode.");
+  });
+}
 
 // ROUTES
 app.use("/api/auth",authRoutes);
+app.use("/keep_alive", (req, res) => {
+  res.send("I am alive");
+});
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Connected to MongoDB");
     
-      app.listen(PORT, () => console.log(`Server is running at Port: ${PORT}`));
+    app.listen(PORT, () => {
+      console.log(`Server is running at Port: ${PORT}`);
+
+
+      const selfPingUrl = `http://localhost:${PORT}/keep_alive`;
+      setInterval(async () => {
+        try {
+          await axios.get(selfPingUrl);
+          console.log(`Self-ping successful at ${new Date().toISOString()}`);
+        } catch (error) {
+          console.error(`Self-ping failed: ${error.message}`);
+        }
+      }, 5 * 60 *  1000);
+    });
     
   })
   .catch((error) => {
