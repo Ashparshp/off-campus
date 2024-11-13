0e9c960dcae9f7f5a1e88f89c209fa50b625e757
Ashwani <ashwanidey2904@gmail.com>
2024-11-13 19:52:23 +0530

    Host Change

--- frontend/src/utility/api.js
+++ frontend/src/utility/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     // baseURL: "http://localhost:3001/api",
     // baseURL: "https://off-campus.onrender.com/api",
-    baseURL : "http://13.232.157.155:3001/api",
+    baseURL : "https://off-campus.in/api",
     // withCredentials: true,
 });
 
