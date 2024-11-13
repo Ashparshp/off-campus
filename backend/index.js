8eb78a786b073b3ba9ce516f40d8e66ebb72bb76
Ashwani <ashwanidey2904@gmail.com>
2024-11-13 19:05:09 +0530

    Served Files

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
